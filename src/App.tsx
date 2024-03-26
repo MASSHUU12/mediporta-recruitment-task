19b60f03ef49d72abe273834d9021429337e7acf
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-26 11:05:21 +0100

    Correct indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
-    return (
-        <div>
-            <p>The quick brown fox jumps over the lazy dog.</p>
-            <p>Sphinx of black quartz, judge my vow.</p>
-        </div>
-    );
+	return (
+		<div>
+			<p>The quick brown fox jumps over the lazy dog.</p>
+			<p>Sphinx of black quartz, judge my vow.</p>
+		</div>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("No element with id 'root' found");
 
 ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
