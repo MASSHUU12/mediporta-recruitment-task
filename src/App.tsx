7961fa044252d65d009a95d465340d61d6388bfe
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-26 11:49:15 +0100

    Setup MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Button from "@mui/material/Button";
+
 function App() {
 	return (
 		<div>
 			<p>The quick brown fox jumps over the lazy dog.</p>
-			<p>Sphinx of black quartz, judge my vow.</p>
+			<Button variant="contained">Sphinx of black quartz, judge my vow.</Button>
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App.tsx";
-import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const rootElement = document.getElementById("root");
 
@@ -9,6 +14,7 @@ if (!rootElement) throw new Error("No element with id 'root' found");
 
 ReactDOM.createRoot(rootElement).render(
 	<React.StrictMode>
+		<CssBaseline />
 		<App />
-	</React.StrictMode>
+	</React.StrictMode>,
 );
