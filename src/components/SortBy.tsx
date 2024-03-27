157c763c445f52e414c40b6a8517b2343dd093ee
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 19:57:36 +0100

    Create SortBy component and its story

---
+++ src/components/SortBy.tsx
@@ -0,0 +1,36 @@
+import { InputLabel, MenuItem, Select, SelectChangeEvent, Stack } from "@mui/material";
+import { useConfigStore } from "../stores/configStore";
+import { SortType } from "../types/Sort";
+
+function SortBy(): JSX.Element {
+	const config = useConfigStore();
+
+	function handleChange(event: SelectChangeEvent) {
+		config.update({
+			...config.config,
+			sort: event.target.value as SortType,
+		});
+	}
+
+	return (
+		<Stack>
+			<InputLabel
+				variant="standard"
+				htmlFor="sort-by"
+			>
+				Sort by
+			</InputLabel>
+			<Select
+				value={config.config.sort}
+				onChange={handleChange}
+				id="sort-by"
+			>
+				<MenuItem value="popular">Popular</MenuItem>
+				<MenuItem value="activity">Activity</MenuItem>
+				<MenuItem value="name">Name</MenuItem>
+			</Select>
+		</Stack>
+	);
+}
+
+export default SortBy;
