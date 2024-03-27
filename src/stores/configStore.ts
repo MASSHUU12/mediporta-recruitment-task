26f6e8560cb0023a114ad494090eeef6ad5cedc1
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 17:55:52 +0100

    Create useConfigStore

---
+++ src/stores/configStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { TagsConfig } from "../types/TagsConfig";
+
+interface ConfigState {
+	config: TagsConfig;
+	update: (config: TagsConfig) => void;
+}
+
+export const useConfigStore = create<ConfigState>()(set => ({
+	config: {
+		page: 1,
+		pageSize: 30,
+		order: "desc",
+		sort: "popular",
+	},
+	update: config => {
+		set({ config });
+	},
+}));
