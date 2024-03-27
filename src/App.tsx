db3ba4b000f00d9b3c7e826223440de08426d121
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 17:41:13 +0100

    Implement changing order

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import Button from "@mui/material/Button";
 import { getTags } from "./classes/SO";
-import { Box, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import TagList from "./components/TagList";
 import { useState } from "react";
 import { TagInfoResponse } from "./types/TagInfo";
+import Menu from "./components/Menu";
+import { TagsConfig } from "./types/TagsConfig";
 
 function App() {
 	const [tags, setTags] = useState<TagInfoResponse | undefined>();
+	const [config, setConfig] = useState<TagsConfig>({
+		page: 1,
+		pageSize: 30,
+		order: "desc",
+		sort: "popular",
+	});
 	const [loading, setLoading] = useState(false);
 	const [isError, setIsError] = useState(false);
 
 	async function loadTags() {
 		setLoading(true);
 
-		await getTags({ page: 1, order: "desc", sort: "popular" })
+		await getTags(config)
 			.then(response => {
 				if (!response.successful) {
 					setIsError(true);
@@ -30,21 +37,29 @@ function App() {
 		setLoading(false);
 	}
 
+	function changeOrder() {
+		if (tags === undefined) return;
+
+		setTags({
+			...tags,
+			items: tags.items.reverse(),
+		});
+	}
+
 	return (
 		<main>
-			<Box
-				display="flex"
-				flexDirection="column"
+			<Stack
 				alignItems="center"
 				justifyContent="center"
 				minHeight="100svh"
+				spacing={2}
+				marginY={5}
 			>
-				<Button
-					variant="contained"
-					onClick={() => void loadTags()}
-				>
-					Sphinx of black quartz, judge my vow.
-				</Button>
+				<Menu
+					submitDisabled={loading}
+					onDirectionChange={changeOrder}
+					onSubmit={() => void loadTags()}
+				/>
 				{tags === undefined && !loading && !isError && (
 					<Typography
 						variant="body2"
@@ -68,7 +83,7 @@ function App() {
 						data={tags}
 					/>
 				)}
-			</Box>
+			</Stack>
 		</main>
 	);
 }
