6e794a3d8a229b7eed7b248a9aea8ae4bb35b1b7
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 20:48:01 +0100

    Remove unused state

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, Stack, Typography } from "@mui/material";
+import { Skeleton, Stack } from "@mui/material";
 import { TagInfoResponse } from "../types/TagInfo";
 import TagInfo from "./TagInfo";
 
@@ -27,14 +27,6 @@ function TagList({ isLoading: dataLoading, data }: TagListProps): JSX.Element {
 
 	return (
 		<Stack spacing={2}>
-			{(data === undefined || data.items.length === 0) && !dataLoading && (
-				<Typography
-					variant="body2"
-					color="text.secondary"
-				>
-					No tags found.
-				</Typography>
-			)}
 			{dataLoading && generateSkeletons(3)}
 			{data !== undefined &&
 				data.items.length !== 0 &&
