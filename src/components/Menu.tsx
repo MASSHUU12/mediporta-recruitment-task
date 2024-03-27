74e43fcde891c77c2a4963a931a37cd0745f94b0
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 17:26:06 +0100

    Create Menu component and its story

---
+++ src/components/Menu.tsx
@@ -0,0 +1,34 @@
+import { Stack } from "@mui/material";
+import { SortOrder } from "../types/Sort";
+import SortDirection from "./SortDirection";
+import Submit from "./Submit";
+import ItemCountField from "./ItemCountField";
+
+interface MenuProps {
+	submitDisabled: boolean;
+	onDirectionChange: (direction: SortOrder) => unknown;
+	onSubmit: () => unknown;
+}
+
+function Menu(props: MenuProps): JSX.Element {
+	return (
+		<Stack
+			spacing={2}
+			direction="row"
+			alignItems="center"
+		>
+			<ItemCountField
+				min={0}
+				max={100}
+				defaultValue={1}
+			/>
+			<SortDirection onClick={props.onDirectionChange} />
+			<Submit
+				disabled={props.submitDisabled}
+				onClick={props.onSubmit}
+			/>
+		</Stack>
+	);
+}
+
+export default Menu;
