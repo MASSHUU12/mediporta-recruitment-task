470d29c51e27f88c05a012dbe3fa0acda57a7f7d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-27 13:38:14 +0100

    Create Submit component with its story

---
+++ src/stories/Submit.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Submit from "../components/Submit";
+
+const meta = {
+	title: "Components/Submit",
+	component: Submit,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
+	args: { disabled: false, onClick: fn() },
+} satisfies Meta<typeof Submit>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {},
+};
